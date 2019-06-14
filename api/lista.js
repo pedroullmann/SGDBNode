56783a177d655d5bd9ca19f37dbd538f5630ad9b
Pedro <pedroullmann@hotmail.com>
2019-06-14 18:09:06 -0300

    mais

--- api/lista.js
+++ api/lista.js
@@ -17,18 +17,51 @@ function criaBlock(path, data) {
     }
 }
 
+function existeArquivo(path) {
+    try {
+        fs.statSync(path)
+        return true
+      }
+      catch (err) {
+          if (err.code === 'ENOENT') {
+              return false
+          }
+      }
+}
+
+function pegaTodosBlocks() {
+    const lista = []
+    const arquivos = fs.readdirSync(listaDiretorio)
+    const arquivosFiltrados = arquivos.filter(junk.not)
+  
+    arquivosFiltrados.forEach(element => {
+      const elementContent = fs.readFileSync(listaDiretorio + element, 'utf8')
+      const json = JSON.parse(elementContent)
+      lista.push(json)
+    })
+
+    return lista
+}
+
+function proximoElementId() {
+    const arquivos = fs.readdirSync(listaDiretorio)
+    const arquivosFiltrados = arquivos.filter(junk.not)
+    const proximoId = arquivosFiltrados.length + 1
+
+    return proximoId
+}
+
 // Module
 module.exports = ( () => {
     return {
         post: ( request, response ) => {
-            const arquivos = fs.readdirSync(listaDiretorio)
-            const arquivosFiltrados = arquivos.filter(junk.not)
-            const proximoId = arquivosFiltrados.length + 1
+            const proximoId = proximoElementId()
 
             const lista = {  
               id: proximoId,
               transacaoBloqueada: request.body.transacaoBloqueada,
-              transacaoLiberada: request.body.transacaoLiberada
+              transacaoLiberada: request.body.transacaoLiberada,
+              deadlock: false
             }
           
             const data = JSON.stringify(lista)
@@ -38,17 +71,46 @@ module.exports = ( () => {
         },
 
         get: ( request, response ) => {
-            const lista = []
-            const arquivos = fs.readdirSync(listaDiretorio)
-            const arquivosFiltrados = arquivos.filter(junk.not)
+            const lista = pegaTodosBlocks()
           
-            arquivosFiltrados.forEach(element => {
-              const elementContent = fs.readFileSync(listaDiretorio + element, 'utf8')
-              const json = JSON.parse(elementContent)
-              lista.push(json)
+            lista.forEach (elementPai => {
+                lista.forEach (elementFilho => {
+                    if (elementPai.id === elementFilho.id) {
+                        return
+                    } else {
+                        const elementPaiPath = `${listaDiretorio}transacao_bloqueada_${elementPai.transacaoBloqueada}.json`
+                        const elementFilhoPath = `${listaDiretorio}transacao_bloqueada_${elementFilho.transacaoBloqueada}.json`
+
+                        if (elementPai.transacaoBloqueada === elementFilho.transacaoLiberada &&
+                            elementPai.transacaoLiberada === elementFilho.transacaoBloqueada && 
+                            existeArquivo(elementPaiPath) && existeArquivo(elementFilhoPath)) {
+                            
+                            try {
+                                fs.unlinkSync(elementPaiPath)
+                                fs.unlinkSync(elementFilhoPath)
+                                const proximoId = proximoElementId()
+
+                                const lista = {  
+                                    id: proximoId,
+                                    transacaoBloqueada: elementPai.transacaoBloqueada,
+                                    transacaoLiberada: elementFilho.transacaoBloqueada,
+                                    deadlock: true
+                                }
+
+                                const data = JSON.stringify(lista)
+                                criaBlock(`${listaDiretorio}transacao_bloqueada_${elementPai.transacaoBloqueada}.json`, data)
+                            } catch(err) {
+                                console.error(err)
+                            }
+                            return
+                        }
+                    }
+                })
             })
-          
-            response.send(lista)
+
+            const newList = pegaTodosBlocks()
+
+            response.send(newList)
         }
     }
 })()
