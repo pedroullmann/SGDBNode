086105403382e00aa23b02c2a39e7e35def681b7
Pedro <pedroullmann@hotmail.com>
2019-06-23 18:08:08 -0300

    ajustes

--- api/checkpoint.js
+++ api/checkpoint.js
@@ -13,8 +13,6 @@ module.exports = ( () => {
             const banco_temporario_file = fs.readFileSync(banco_temporario_diretorio)
             fs.writeFileSync(banco_diretorio, banco_temporario_file)
 
-            console.log(fs.readFileSync(banco_diretorio))
-
             response.send(true)
         }
     }
